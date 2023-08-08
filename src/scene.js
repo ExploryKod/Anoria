06b27114ad2d734296b9d445a3af9567b6c9e1b7
Amaury Franssen <amauryfranssen@gmail.com>
2023-08-08 21:22:18 +0200

    feat(buildings): basic structure to add buildings

--- src/scene.js
+++ src/scene.js
@@ -12,10 +12,54 @@ export function createScene() {
     renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
     gameWindow.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1,1,1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+ 
+
+    let meshes = [];
+    function initialize(city) { 
+        scene.clear();
+        meshes = [];
+        for(let x = 0; x < city.size; x++) {
+            let column = [];
+            for(let y = 0; y < city.size; y++) {
+                // Grass
+                const geometry = new THREE.BoxGeometry(1,1,1);
+                const material = new THREE.MeshLambertMaterial({ color: 0x00aa00 });
+                const mesh = new THREE.Mesh(geometry, material);
+                mesh.position.set(x, -0.5, y);
+                scene.add(mesh);
+                column.push(mesh);
+
+                // Building
+                const tile = city.data[x][y];
+                if(tile.building === 'building') {
+                const buildingGeometry = new THREE.BoxGeometry(1,1,1);
+                const materialGeometry = new THREE.MeshLambertMaterial({ color: 0x777777 });
+                const meshGeometry = new THREE.Mesh(buildingGeometry, materialGeometry);
+                meshGeometry.position.set(x, 0.5, y);
+                scene.add(meshGeometry);
+                column.push(meshGeometry);
+                }
+            }
+            meshes.push(column);
+        }
+
+        setUpLights();
+    }
+
+    function setUpLights() {
+        const lights = [
+            new THREE.AmbientLight(0xffffff, 0.2),
+            new THREE.DirectionalLight(0xffffff, 0.3),
+            new THREE.DirectionalLight(0xffffff, 0.3),
+            new THREE.DirectionalLight(0xffffff, 0.3)
+        ];
+
+        lights[1].position.set(0, 1, 0);
+        lights[2].position.set(0, 1, 0);
+        lights[3].position.set(0, 1, 0);
+
+        scene.add(...lights);
+    }
 
     function draw() {
         renderer.render(scene, camera.camera);
@@ -54,6 +98,7 @@ export function createScene() {
     }
 
     return {
+        initialize,
         start,
         stop,
         onMouseDown,
@@ -61,6 +106,7 @@ export function createScene() {
         onMouseMove, 
         onKeyBoardDown,
         onKeyBoardUp,
+        setUpLights,
         onKeyBoardStay
     }
 }
