4fbfb8ab5f55c4d5b984d7293fe09713622bffae
Amaury Franssen <amauryfranssen@gmail.com>
2023-08-10 22:21:54 +0200

    feat(component): factory functions basic structure

---
+++ src/asset.js
@@ -0,0 +1,9 @@
+import * as THREE from 'three';
+
+// Object of factory and anonymous functions for creating assets
+const assets = {
+    'grass': (x, y) => {},
+    'building-1': (x, y) => {},
+    'building-2': (x, y) => {},
+    'building-3': (x, y) => {}
+}
