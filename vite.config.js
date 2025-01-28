9b825aa312d21f6ff8038ffe04420bba2eda38b8
Amaury Franssen <amauryfranssen@gmail.com>
2025-01-28 01:00:37 +0100

    add outdir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
+
   plugins: [VitePWA({
     registerType: 'prompt',
     injectRegister: true,
