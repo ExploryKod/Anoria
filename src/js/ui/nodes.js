be18e9cffb5fffd19b03d028e7565873da196e25
Amaury Franssen <amauryfranssen@gmail.com>
2025-02-22 14:51:19 +0100

    feat(styles): change style for layout

--- src/js/ui/nodes.js
+++ src/js/ui/nodes.js
@@ -1,6 +1,6 @@
 // game.js
-export const displayTime = document.querySelector('.info-panel .display-time');
-export const displaySpeed = document.querySelector('.info-panel .display-speed');
+export const displayTime = document.querySelector('.display-time');
+export const displaySpeed = document.querySelector('.display-speed');
 export const overOverlay = document.querySelector('#over-overlay');
 export const overOverlayMessage = document.querySelector('#over-overlay .over-overlay__text');
 export const infoObjectOverlay = document.querySelector('.info-building-overlay');
@@ -10,18 +10,18 @@ export const buildingsObjects = [
     'Tombstone-1', 'Farm-Carrot', 'Farm-Wheat', 'Farm-Cabbage'
 ];
 
-export const infoPanelClock = document.querySelector('.info-panel .clock-box');
-export const infoPanelClockIcon = document.querySelector('.info-panel svg.lucide-clock-4')
-export const infoPanelNoClockIcon = document.querySelector('.info-panel svg.lucide-alarm-clock-off')
-export const delayBox = document.querySelector('.info-panel .delay-box');
+export const infoPanelClock = document.querySelector(' .clock-box');
+export const infoPanelClockIcon = document.querySelector(' svg.lucide-clock-4')
+export const infoPanelNoClockIcon = document.querySelector(' svg.lucide-alarm-clock-off')
+export const delayBox = document.querySelector(' .delay-box');
 // scene.js
 export const gameWindow = document.getElementById('game-window');
-export const displayPop = document.querySelector('.info-panel .display-pop');
-export const displayDelay = document.querySelector('.info-panel .display-delay');
+export const displayPop = document.querySelector(' .display-pop');
+export const displayDelay = document.querySelector(' .display-delay');
 export const displayDelayUI = document.querySelector('.delay-ui');
 export const bulldozeSelected = document.querySelector('.bulldoze-btn');
-export const displayFunds = document.querySelector('.info-panel .display-funds');
-export const displayDebt = document.querySelector('.info-panel .display-debt');
+export const displayFunds = document.querySelector(' .display-funds');
+export const displayDebt = document.querySelector(' .display-debt');
 export const houses = ['House-Red', 'House-Purple', 'House-Blue', 'House-2Story'];
 export const firstHouses = ['House-Red', 'House-Purple', 'House-Blue'];
 export const bigHouses = ['House-2Story'];
